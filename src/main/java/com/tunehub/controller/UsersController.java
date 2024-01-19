69435eedd88c3136a274672d6825ff224101b533
MAHESH <MAHESH@DESKTOP-5H6COCL>
2024-01-19 16:03:53 +0530

    5th Version of TuneHub

--- src/main/java/com/tunehub/controller/UsersController.java
+++ src/main/java/com/tunehub/controller/UsersController.java
@@ -1,7 +1,5 @@
 package com.tunehub.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.tunehub.entities.Song;
 import com.tunehub.entities.Users;
-import com.tunehub.services.SongService;
 import com.tunehub.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
